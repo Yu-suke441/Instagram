e4670a44a1b74420e54872d31df807d59ed1b3e9
Yu-suke441 <it.creative.pgm.738@gmail.com>
2020-11-19 13:41:53 +0900

    Instaguraクローン作成を開始

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Yusuke Murayama on 2020/11/19.
+//  Copyright © 2020 Yusuke.Murayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
